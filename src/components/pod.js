2c461d5c94694336cf6d664c35f44338d06e3ca7
Satnam Sandhu <SSandhu@rightpoint.com>
2020-04-27 20:25:16 -0500

    change read more button color

--- src/components/pod.js
+++ src/components/pod.js
@@ -19,12 +19,14 @@ class PhotoOfDay extends React.Component {
                 <div className=" col-lg-offset-4 col-lg-8">
                     <h3 className="top-left">PIC OF THE DAY</h3>
                     <img src={this.state.photo.url} alt={this.state.photo.title} />
-                    {/* <div className="bottom-right">{this.state.photo.explanation}</div> */}
-                    {this.state.photo !== ""  && <div className="text-block"><ReadMoreReact text={this.state.photo.explanation}
-                        min={200}
-                        ideal={250}
-                        max={300}
-                        readMoreText="Read more" /></div>}
+                    <div className="text-block">
+                        {this.state.photo !== "" &&
+                            <ReadMoreReact text={this.state.photo.explanation}
+                                min={200}
+                                ideal={250}
+                                max={300}
+                                readMoreText="Read more" />}
+                    </div>
                 </div>
             </div>
         </div>

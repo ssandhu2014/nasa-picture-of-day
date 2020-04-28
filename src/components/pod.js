58c677c82f3c3c2471a7da3b5104e99de001f90f
Satnam Sandhu <SSandhu@rightpoint.com>
2020-04-27 20:13:53 -0500

    nasa picture of the day code

---
+++ src/components/pod.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReadMoreReact from 'read-more-react';
+
+
+class PhotoOfDay extends React.Component {
+    state = {
+        photo: ""
+    };
+
+    componentDidMount() {
+        fetch("https://api.nasa.gov/planetary/apod?api_key=6EZSieImreoe3f5TIqcBYcVvbLoLvWvZvZdOfsVQ")
+            .then(response => response.json())
+            .then(json => this.setState({ photo: json }));
+    }
+
+    render() {
+        return <div className="container">
+            <div className="row">
+                <div className=" col-lg-offset-4 col-lg-8">
+                    <h3 className="top-left">PIC OF THE DAY</h3>
+                    <img src={this.state.photo.url} alt={this.state.photo.title} />
+                    {/* <div className="bottom-right">{this.state.photo.explanation}</div> */}
+                    {this.state.photo !== ""  && <div className="text-block"><ReadMoreReact text={this.state.photo.explanation}
+                        min={200}
+                        ideal={250}
+                        max={300}
+                        readMoreText="Read more" /></div>}
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default PhotoOfDay;
